0c11659e7fba86692bb6807d1f5293ec53293249
Smallbunz <45081047+Smallbunz@users.noreply.github.com>
2020-12-20 20:02:03 +0000

    play button

---
+++ Assets/Scripts/PlayManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayManager : MonoBehaviour
+{
+	//[SerializedField] private string GameScene;
+	
+	//look for string (scene) named GameScene
+	public void PlayButtonMoveScene(string GameScene)
+	{
+		SceneManager.LoadScene(GameScene);
+	}
+
+}
